59305ff33aeaa23ecb8fe6b1bdea98b42eca53e5
giriprakashk <101519714+giriprakashk@users.noreply.github.com>
2022-05-13 16:28:00 +0530

    commit

--- 3_Implementation/Debug/sources.mk
+++ 3_Implementation/Debug/sources.mk
@@ -21,6 +21,10 @@ C_DEPS :=
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
-Src \
-Startup \
-
+Core/Src \
+Core/Startup \
+Drivers/STM32F4xx_HAL_Driver/Src \
+Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Src \
+Middlewares/ST/STM32_USB_Host_Library/Core/Src \
+USB_HOST/App \
+USB_HOST/Target \
